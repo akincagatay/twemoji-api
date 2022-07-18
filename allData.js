bf613fac0984ef69d0b6d50e7aab41f976f0abda
akincagatay <akincagataycaliskan@gmail.com>
2022-07-18 16:01:29 +0300

    main

--- allData.js
+++ allData.js
@@ -16,34 +16,68 @@ router.get("/",async (req, res) =>{
  // Get likely
  router.get('/name/:name',(req,res)=>
  {
-    var name = req.params.name;
-    let respondList = new Array();
-    for(let i = 0; i < emoji_data.length; i++) 
-    {  
-        let item = emoji_data[i];
-        console.log(item)
-        if(item.name.includes(name.toLowerCase()))
-        {
-            respondList.push(item) 
-        }     
+    try
+    {
+        var name = req.params.name;
+        let respondList = new Array();
+        for(let i = 0; i < emoji_data.length; i++) 
+        {  
+            let item = emoji_data[i];
+            if(item.name.includes(name.toLowerCase()))
+            {
+                respondList.push(item); 
+            }     
+        }
+        res.status(200).json(respondList);   
     }
-	res.status(200).json(respondList);         
+    catch(err){
+        res.status(500).json(err);
+    }
+          
  })
 
- router.get('/category/:category',(req,res)=>
+ router.get('/category/:category',async (req,res)=>
  {
-    var category = req.params.category;
-    let respondList = new Array();
-    for(let i = 0; i < emoji_data.length; i++) 
-    {  
-        let item = emoji_data[i];
-        console.log(item)
-        if(item.category.includes(category.toLowerCase()))
-        {
-            respondList.push(item) 
-        }     
+    try
+    {
+        var category = req.params.category;
+        let respondList = new Array();
+        for(let i = 0; i < emoji_data.length; i++) 
+        {  
+            let item = emoji_data[i];
+            if(item.category.includes(category.toLowerCase()))
+            {
+                respondList.push(item) 
+            }     
+        }
+        res.status(200).json(respondList);    
     }
-	res.status(200).json(respondList);         
+    catch(err){
+        res.status(500).json(err);
+    }
+         
+ })
+
+ router.get('/subcategory/:subcategory',async (req,res)=>
+ {
+    try
+    {
+        var sub_category = req.params.sub-category;
+        let respondList = new Array();
+        for(let i = 0; i < emoji_data.length; i++) 
+        {  
+            let item = emoji_data[i];
+            if(item.category.includes(sub_category.toLowerCase()))
+            {
+                respondList.push(item) 
+            }     
+        }
+        res.status(200).json(respondList);    
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+         
  })
 
 module.exports = router;
