dcf216a9037f86bb1d904f5cdb9e31b4ef12402e
akincagatay <akincagataycaliskan@gmail.com>
2022-07-18 16:43:51 +0300

    main

--- data.js
+++ data.js
@@ -1,3 +1,5 @@
 var fs=require('fs');
 let rawdata = fs.readFileSync('emoji_data.json');
-export const emoji_data = JSON.parse(rawdata);
+const emoji_data = JSON.parse(rawdata);
+
+export default emoji_data;
